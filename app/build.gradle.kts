1796b079b24ec8a003bdcbadc634b11c036e13bd
RooaaShanshal <shanshar@mcmaster.ca>
2024-04-08 01:14:45 -0400

    Attempted Encryption again
    Using Java (instead of Kotlin version in branch)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.7.7")
     implementation("androidx.annotation:annotation:1.7.1")
     implementation("com.google.firebase:firebase-messaging:23.4.1")
+    implementation("com.google.firebase:firebase-database:20.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
