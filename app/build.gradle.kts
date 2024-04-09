e4cba0d75727bf08f21de84b9326890ddead2baf
Derron Li <104483680+derronli@users.noreply.github.com>
2024-04-09 01:44:58 -0400

    Kdc management class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.louxiaotian"
-        minSdk = 34
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
