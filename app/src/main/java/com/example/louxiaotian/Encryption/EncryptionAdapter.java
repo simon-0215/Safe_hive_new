9013913a39a5b75c72efb79b365ef357b8e3f208
RooaaShanshal <shanshar@mcmaster.ca>
2024-04-09 10:09:25 -0400

    Encryption Adapter Completed

--- app/src/main/java/com/example/louxiaotian/Encryption/EncryptionAdapter.java
+++ app/src/main/java/com/example/louxiaotian/Encryption/EncryptionAdapter.java
@@ -1,11 +1,12 @@
 package com.example.louxiaotian.Encryption;
-import com.example.louxiaotian.Encryption.EncryptionManager;
+
+import com.example.louxiaotian.EncryptionManager;
+
 import java.io.InputStream;
 import java.io.OutputStream;
-public class EncryptionConductor {
 
+public class EncryptionAdapter {
     static EncryptionManager em = new EncryptionManager();
-
 
     public static byte[] encrypt(byte[] ByteArray, OutputStream outputStream){
         return em.encrypt(ByteArray, outputStream);
